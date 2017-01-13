3ff90fc596417d2f91e684b6c9b07adc244fc1e1
sebdak <sebastdk@stud.ntnu.no>
2017-01-13 11:15:53 +0100

    update hello-world.py

--- exercises/exercise_1/hello-world.py
+++ exercises/exercise_1/hello-world.py
@@ -4,23 +4,36 @@
 
 from threading import Thread
 
-i = 0
+count = 0
 
 def someThreadFunction():
+    global count
     print("Hello from a thread!")
+    for i in xrange(1000000):
+    	count+=1
 
+
+def someThreadFunction2():
+    global count
+    print("Hello from a thread!")
+    for i in xrange(1000000):
+    	count-=1
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
-    global i
+    
 
 
 def main():
     someThread = Thread(target = someThreadFunction, args = (),)
+    someThread2 = Thread(target = someThreadFunction2, args = (),)
+
     someThread.start()
-    
+    someThread2.start()
+
     someThread.join()
-    print("Hello from main!")
+    someThread2.join()
+    print("Count equals: ", count)
 
 
 main()
