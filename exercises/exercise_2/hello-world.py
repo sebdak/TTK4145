4169a8ced045519bf0d87304bbf3a7675fb645fd
sebdak <sebastdk@stud.ntnu.no>
2017-01-20 10:34:17 +0100

    fix concurrency problems

--- exercises/exercise_2/hello-world.py
+++ exercises/exercise_2/hello-world.py
@@ -2,22 +2,26 @@
 # Python 3.3.3 and 2.7.6
 # python helloworld_python.py
 
-from threading import Thread
+import threading
 
 count = 0
+lock = threading.Lock()
 
 def someThreadFunction():
     global count
+    lock.acquire()
     print("Hello from a thread!")
     for i in xrange(1000000):
     	count+=1
-
+    lock.release()
 
 def someThreadFunction2():
     global count
+    lock.acquire()
     print("Hello from a thread!")
     for i in xrange(1000000):
-    	count-=1
+        count-=1
+    lock.release()
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
@@ -25,8 +29,8 @@ def someThreadFunction2():
 
 
 def main():
-    someThread = Thread(target = someThreadFunction, args = (),)
-    someThread2 = Thread(target = someThreadFunction2, args = (),)
+    someThread = threading.Thread(target = someThreadFunction, args = (),)
+    someThread2 = threading.Thread(target = someThreadFunction2, args = (),)
 
     someThread.start()
     someThread2.start()
