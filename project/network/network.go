76bd0a0f12d4ed68054699a1a1c3f4a9e3112cae
smyrberg <sondre.myrberg@gmail.com>
2017-03-14 02:24:48 +0100

    removed ":"

--- project/network/network.go
+++ project/network/network.go
@@ -180,7 +180,7 @@ func testIfOnline() bool {
 }
 
 func listenForMaster(){
-	masterRx := make(chan string,1)
+	masterRx = make(chan string,1)
 	go bcast.Receiver(constants.MasterPort, masterRx)
 }
 
